232c8be15dce33e07f5905e9941ce35499f2543a
Alejandro <averagonzalez@omnitech-inc.com>
2021-01-04 13:38:09 -0600

    Train Model Function

--- FraudDetection/Program.cs
+++ FraudDetection/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Common;
+using FraudDetection.DataStructures;
 using Microsoft.ML;
+using Microsoft.ML.Data;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace FraudDetection
 {
@@ -54,11 +57,72 @@ namespace FraudDetection
             var metrics = mlContext.BinaryClassification.Evaluate(data: predictions,
                                                                     labelColumnName: nameof(TransactionData.IsFraud),
                                                                     scoreColumnName: "Score");
+
+            ConsoleHelper.PrintBinaryClassificationMetrics(trainerName, metrics);
         }
 
         private static (ITransformer model, string trainerName) TrainModel(MLContext mlContext, IDataView trainingDataView)
         {
-            throw new NotImplementedException();
+            string[] featureColumnNames = trainingDataView.Schema.AsQueryable()
+                .Select(column => column.Name)
+                .Where(name => name != nameof(TransactionData.IsFraud))
+                .Where(name => name != "IdPreservationColumn")
+                .Where(name => name != "Type")
+                .Where(name => name != "NameOrigin")
+                .Where(name => name != "NameDest")
+                .Where(name => name != "IsFlaggedFraud")
+                .ToArray();
+
+            var dataProcessPipeline = mlContext.Transforms.Concatenate("Features", featureColumnNames)
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("TypeOneHotEncoded", "Type"))
+                .Append(mlContext.Transforms.DropColumns(new string[] { "Type", "NameOrigin", "NameDest", "IsFlaggedFraud" }));
+
+            ConsoleHelper.PeekDataViewInConsole(mlContext, trainingDataView, dataProcessPipeline, 5);
+
+            var trainer = mlContext.BinaryClassification.Trainers.LightGbm(labelColumnName: nameof(TransactionData.IsFraud),
+                                                                                numberOfLeaves: 25,
+                                                                                numberOfIterations: 225,
+                                                                                minimumExampleCountPerLeaf: 20,
+                                                                                learningRate: .001);
+
+            IDataView oneHotEncodedData = dataProcessPipeline.Fit(trainingDataView).Transform(trainingDataView);
+
+            var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+            PrintDataColumn(oneHotEncodedData, "TypeOneHotEncoded");
+
+            ConsoleHelper.ConsoleWriteHeader("==========Training Model=========");
+
+            var model = trainingPipeline.Fit(trainingDataView);
+
+            ConsoleHelper.ConsoleWriteHeader("==========End of Training========");
+
+            var fccPipeline = model.Append(mlContext.Transforms.CalculateFeatureContribution(model.LastTransformer).Fit(dataProcessPipeline.Fit(trainingDataView).Transform(trainingDataView)));
+
+            return (fccPipeline, fccPipeline.ToString());
+        }
+
+        private static void PrintDataColumn(IDataView encodedData, string columnName)
+        {
+            var countSelectColumn = encodedData.GetColumn<float[]>(encodedData.Schema[columnName]);
+
+            ConsoleHelper.ConsoleWriteHeader("==========One Hot Encoding Results=========");
+
+            int j = 0;
+            foreach (var row in countSelectColumn)
+            {
+                for (int i = 0; i < row.Length; i++)
+                    Console.Write($"{row[i]}\t");
+
+                Console.WriteLine();
+
+                j++;
+
+                if (j > 10) { break; }
+                {
+
+                }
+            }
         }
 
         private static void PrepDatasets(MLContext mlContext, string fullDatasetPath, string trainDatasetPath, string testDatasetPath)
